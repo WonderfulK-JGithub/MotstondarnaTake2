1673767c659ded6d41afdd8c2b34413c20279da9
angulan <79506187+angulan@users.noreply.github.com>
2022-02-17 14:02:26 +0100

    🍆🍑😍😍😍

---
+++ Motstondarna GruppProjekt/Assets/Scripts/Editor scripts/SortChildren.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class SortChildren : MonoBehaviour
+{
+
+	[ContextMenu("Sort")]
+	public void SortChildrenByName()
+	{
+		foreach (GameObject obj in Selection.gameObjects)
+		{
+			List<Transform> children = new List<Transform>();
+			for (int i = obj.transform.childCount - 1; i >= 0; i--)
+			{
+				Transform child = obj.transform.GetChild(i);
+				children.Add(child);
+				child.parent = null;
+			}
+			children.Sort((Transform t1, Transform t2) => { return t1.name.CompareTo(t2.name); });
+			foreach (Transform child in children)
+			{
+				child.parent = obj.transform;
+			}
+		}
+		Debug.Log("Sorted!");
+
+	}
+}
