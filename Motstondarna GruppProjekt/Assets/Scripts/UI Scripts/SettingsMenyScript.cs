485eace17c1749a0a6c4bb0887ee025dab756270
WonderfulK-JGithub <Wonderfulkjr@gmail.com>
2022-03-08 21:52:28 +0100

    fps cap

--- Motstondarna GruppProjekt/Assets/Scripts/UI Scripts/SettingsMenyScript.cs
+++ Motstondarna GruppProjekt/Assets/Scripts/UI Scripts/SettingsMenyScript.cs
@@ -9,6 +9,7 @@ public class SettingsMenyScript : MonoBehaviour
     private void Start()
     {
         AudioListener.volume = PlayerPrefs.GetFloat("musicVolume", 1);
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
     }
 
     public void Settings() //Settings meny knappen
